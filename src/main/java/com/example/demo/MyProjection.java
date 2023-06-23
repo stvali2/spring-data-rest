144cefa6f4336d39a8d3fed48148ab8ad006a21c
Valentin Petras <stvali2@gmail.com>
2023-06-23 20:48:18 +0200

    ⬆️ updates dependencies and ♻️ refactors the app

--- src/main/java/com/example/demo/MyProjection.java
+++ src/main/java/com/example/demo/MyProjection.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.data.rest.core.config.Projection;
 
-@Projection(name = "My", types = {User.class})
+@Projection(name = "my", types = {User.class})
 public interface MyProjection {
     Address getAddress();
 }
--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface UserRespository extends MongoRepository<User, ObjectId> {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
 }
